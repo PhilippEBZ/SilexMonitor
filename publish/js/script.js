6f576772d5aa1bbbd0f24c6b3853f08bbb228280
PhilippEBZ <33900993+PhilippEBZ@users.noreply.github.com>
2017-11-24 14:36:12 +0100

    Create publish/js/script.js

--- publish/js/script.js
+++ publish/js/script.js
@@ -1,3 +1,9 @@
 
+        $(function(){
+            // here, the website is loaded
+            // this will fade the body out and then fade in again
+            $('body').hide(2000).show(2000);
+        });
+        
         <script type="text/javascript" src="https://raw.githubusercontent.com/PhilippEBZ/SilexMonitor/master/smoothie.js">
     
